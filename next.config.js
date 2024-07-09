74162b5b6bb9d3100768f498b7aba4358083c01e
Stavros <kalfas@protonmail.com>
2024-07-09 20:45:57 +0300

    favico and output in next config

--- next.config.js
+++ next.config.js
@@ -3,13 +3,15 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 });
 
-module.exports = withBundleAnalyzer({
+/** @type {import('next').NextConfig} */
+const nextConfig = withBundleAnalyzer({
   poweredByHeader: false,
   trailingSlash: true,
   // The starter code loads resources from `public` folder with `router.basePath` in React components.
   // So, the source code is "basePath-ready".
   // You can remove `basePath` if you don't need it.
   reactStrictMode: true,
+  output: 'export',
   exportPathMap: async () => ({
     '/': { page: '/' },
     '/terms': { page: '/terms' },
@@ -17,3 +19,4 @@ module.exports = withBundleAnalyzer({
     // Add other paths here if you have additional pages
   }),
 });
+module.exports = nextConfig;
