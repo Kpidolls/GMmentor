4fd2dcccfc7840095fbaa9b996bc038dfd42c92a
Stavros <kalfas@protonmail.com>
2024-07-10 10:35:20 +0300

    config

--- next.config.js
+++ next.config.js
@@ -13,4 +13,12 @@ module.exports = withBundleAnalyzer({
   // So, the source code is "basePath-ready".
   // You can remove `basePath` if you don't need it.
   reactStrictMode: true,
+  async exportPathMap() {
+    return {
+      '/': { page: '/' },
+      '/terms': { page: '/terms' },
+      '/privacy-policy': { page: '/privacy-policy' },
+      // Add other paths here if you have additional pages
+    };
+  },
 });
