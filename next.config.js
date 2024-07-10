b2835a05756bcd8aa36ff5ebd90b83d5dff7dddd
Stavros <kalfas@protonmail.com>
2024-07-10 10:03:47 +0300

    next config and package.json changes

--- next.config.js
+++ next.config.js
@@ -1,23 +1,16 @@
+const isProd = process.env.NODE_ENV === 'production';
 /* eslint-disable import/no-extraneous-dependencies */
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 });
 
-/** @type {import('next').NextConfig} */
-const nextConfig = withBundleAnalyzer({
-  basePath: '/googlementor.com',
+module.exports = withBundleAnalyzer({
   poweredByHeader: false,
   trailingSlash: true,
-  // The starter code loads resources from `public` folder with `router.basePath` in React components.
+  basePath: isProd ? '/GMmentor' : '',
+  assetPrefix: isProd ? '/GMmentor/' : '',
+  // The starter code load resources from `public` folder with `router.basePath` in React components.
   // So, the source code is "basePath-ready".
   // You can remove `basePath` if you don't need it.
   reactStrictMode: true,
-  output: 'export',
-  exportPathMap: async () => ({
-    '/': { page: '/' },
-    '/terms': { page: '/terms' },
-    '/privacy-policy': { page: '/privacy-policy' },
-    // Add other paths here if you have additional pages
-  }),
 });
-module.exports = nextConfig;
