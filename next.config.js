8051fe50d2a5c78881a053e2aa1ad55f62648faf
Stavros <kalfas@protonmail.com>
2024-07-10 12:27:14 +0300

    nextjs configuration and package.json

--- next.config.js
+++ next.config.js
@@ -1,14 +1,13 @@
-const isProd = process.env.NODE_ENV === 'production';
 /* eslint-disable import/no-extraneous-dependencies */
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 });
 
-module.exports = withBundleAnalyzer({
+/** @type {import('next').NextConfig} */
+const nextConfig = withBundleAnalyzer({
   poweredByHeader: false,
   trailingSlash: true,
-  basePath: isProd ? '/GMmentor' : '',
-  assetPrefix: isProd ? '/GMmentor/' : '',
+  output: 'export',
   // The starter code load resources from `public` folder with `router.basePath` in React components.
   // So, the source code is "basePath-ready".
   // You can remove `basePath` if you don't need it.
@@ -22,3 +21,5 @@ module.exports = withBundleAnalyzer({
     };
   },
 });
+
+module.exports = nextConfig;
