c452196306e01f522887567f2cc524aac085450c
Stavros Kalfas <kalfas@protonmail.com>
2024-10-10 13:49:24 +0300

    configured deploy

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,8 +2,8 @@ import { AppProps } from 'next/app';
 
 import '../styles/main.css';
 
-const MyApp = ({ Component, pageProps }: AppProps) => (
-  <Component {...pageProps} />
-);
+const MyApp = ({ Component, pageProps }: AppProps) => {
+  return <Component {...pageProps} />;
+};
 
 export default MyApp;
