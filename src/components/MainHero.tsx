bc649b8c44cc2a919d3d8cf116f801a1a8e0ca46
Stavros Kalfas <kalfas@protonmail.com>
2024-10-24 12:32:08 +0300

    Refactor MainHero component and update color scheme

--- src/components/MainHero.tsx
+++ src/components/MainHero.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-
 import config from '../config/index.json';
 
 const MainHero = () => {
@@ -9,7 +8,7 @@ const MainHero = () => {
       <div className="sm:text-center lg:text-left">
         <h1 className="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
           <span className="block xl:inline">{mainHero.title}</span>{' '}
-          <span className={`block text-primary xl:inline`}>
+          <span className="block text-indigo-600 xl:inline">
             {mainHero.subtitle}
           </span>
         </h1>
@@ -20,7 +19,7 @@ const MainHero = () => {
           <div className="rounded-md shadow">
             <a
               href={mainHero.primaryAction.href}
-              className={`w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-background bg-primary hover:bg-border hover:text-primary md:py-4 md:text-lg md:px-10`}
+              className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 md:py-4 md:text-lg md:px-10"
             >
               {mainHero.primaryAction.text}
             </a>
@@ -28,7 +27,7 @@ const MainHero = () => {
           <div className="mt-3 sm:mt-0 sm:ml-3">
             <a
               href={mainHero.secondaryAction.href}
-              className={`w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md border-primary text-secondary bg-background hover:bg-border hover:text-primary md:py-4 md:text-lg md:px-10`}
+              className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-indigo-600 bg-white hover:bg-gray-50 md:py-4 md:text-lg md:px-10"
             >
               {mainHero.secondaryAction.text}
             </a>
