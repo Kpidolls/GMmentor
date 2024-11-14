d1bfa05d8b002e68aee4fa2044e309a2ee9f6e60
Stavros Kalfas <kalfas@protonmail.com>
2024-11-14 19:32:21 +0200

    Refactor Next.js configuration and components for improved structure; remove unused code, add SEO metadata, and enhance page titles for better visibility.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,16 @@
 import { AppProps } from 'next/app';
-
+import Head from 'next/head';
 import '../styles/main.css';
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>Google Mentor - Enhance Your Travel Experience with custom maps</title>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 };
 
 export default MyApp;
