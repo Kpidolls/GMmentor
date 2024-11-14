cd2e63ca9957958d941c218f1b7d1fd9155cd6b1
Stavros Kalfas <kalfas@protonmail.com>
2024-11-14 18:47:19 +0200

    Refactor About and AboutUs components for improved mission and values descriptions; update eSim to eSIM and enhance clarity. Add SEO metadata to _document for better search visibility.

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,7 +7,20 @@ class MyDocument extends Document {
   render() {
     return (
       <Html lang={AppConfig.locale}>
-        <Head />
+        <Head>
+        <title>Google Mentor - Enhance Your Travel Experience with custom maps</title>
+        <meta name="description" content="Discover our range of services designed to enhance your traveling. From travel insurance to mobile, we have you covered." />
+        <meta name="keywords" content="travel insurance, mobile data e-sim, mapping services, Google Maps, tourist guides" />
+        <meta name="author" content="Travel Tips" />
+        <meta property="og:title" content="Google Mentor - Enhance Your Experience with our custom maps" />
+        <meta property="og:description" content="Discover our range of services designed to enhance your traveling. From travel insurance to mobile, we have you covered." />
+        <meta property="og:image" content="/assets/images/newlogo1.png" />
+        <meta property="og:url" content="https://www.googlementor.com" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="Google Mentor - Enhance Your Experience with our custom maps" />
+        <meta name="twitter:description" content="Discover our range of services designed to enhance your traveling. From travel insurance to mobile, we have you covered." />
+        <meta name="twitter:image" content="/assets/images/newlogo1.png" />
+        </Head>
         <body>
           <Main />
           <NextScript />
