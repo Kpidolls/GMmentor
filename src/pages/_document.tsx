06ce664171e11af5a30e296bfce5e11658598ce8
Stavros Kalfas <kalfas@protonmail.com>
2024-11-14 19:42:49 +0200

    Update next-seo package to version 6.6.0; add canonical link and enhance AppConfig for improved SEO and service description.

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -8,6 +8,7 @@ class MyDocument extends Document {
     return (
       <Html lang={AppConfig.locale}>
         <Head>
+        <link rel="canonical" href={AppConfig.baseUrl} />
         <meta name="description" content="Discover our range of services designed to enhance your traveling. From travel insurance to mobile, we have you covered." />
         <meta name="keywords" content="travel insurance, mobile data e-sim, mapping services, Google Maps, tourist guides" />
         <meta name="author" content="Travel Tips" />
