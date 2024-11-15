07d7a21159eccca475f731459e74e51f89bf320d
Stavros Kalfas <kalfas@protonmail.com>
2024-11-15 10:57:55 +0200

    Update SEO metadata in _document for improved clarity and relevance

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -9,16 +9,16 @@ class MyDocument extends Document {
       <Html lang={AppConfig.locale}>
         <Head>
         <link rel="canonical" href={AppConfig.baseUrl} />
-        <meta name="description" content="Discover our range of services designed to enhance your traveling. From travel insurance to mobile, we have you covered." />
+        <meta name="description" content="Enhance your traveling. From travel insurance to mobile data e-SIMS, we have you covered." />
         <meta name="keywords" content="travel insurance, mobile data e-sim, mapping services, Google Maps, tourist guides" />
         <meta name="author" content="Travel Tips" />
-        <meta property="og:title" content="Google Mentor - Enhance Your Experience with our custom maps" />
-        <meta property="og:description" content="Discover our range of services designed to enhance your traveling. From travel insurance to mobile, we have you covered." />
+        <meta property="og:title" content="Google Mentor - custom maps" />
+        <meta property="og:description" content="Enhance your traveling. From travel insurance to mobile data e-SIMS, we have you covered." />
         <meta property="og:image" content="/assets/images/newlogo1.png" />
         <meta property="og:url" content="https://www.googlementor.com" />
         <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:title" content="Google Mentor - Enhance Your Experience with our custom maps" />
-        <meta name="twitter:description" content="Discover our range of services designed to enhance your traveling. From travel insurance to mobile, we have you covered." />
+        <meta name="twitter:title" content="Google Mentor - Enhance Your Experience with custom maps" />
+        <meta name="twitter:description" content="Enhance your traveling. From travel insurance to mobile data e-SIMS, we have you covered." />
         <meta name="twitter:image" content="/assets/images/newlogo1.png" />
         </Head>
         <body>
