b8c9a056c546a95cbdbd917519561a5c97431240
Stavros Kalfas <kalfas@protonmail.com>
2024-12-18 21:41:28 +0200

    Update configuration and app settings

--- src/utils/AppConfig.ts
+++ src/utils/AppConfig.ts
@@ -1,7 +1,7 @@
 export const AppConfig = {
-  site_name: 'Google Mentor',
-  title: 'Google Mentor',
-  description: 'Discover our range of services designed to enhance your traveling. From travel insurance to mobile, we have you covered.',
+  site_name: 'Googlementor',
+  title: 'Googlementor',
+  description: 'Travel like a pro with location suggestions for your maps.',
   locale: 'en',
   baseUrl: process.env.NODE_ENV === 'production' ? 'https://kpidolls.github.io/GMmentor/' : 'http://localhost:3000',
 };
