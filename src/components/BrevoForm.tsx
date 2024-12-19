eadebdb131be2a9a4f0a9fb79938c6153e0a3be5
Stavros Kalfas <kalfas@protonmail.com>
2024-12-19 13:12:59 +0200

    Add BrevoForm component and integrate it into the main application layout

---
+++ src/components/BrevoForm.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const BrevoForm = () => {
+  return (
+    <div className="sib-form" style={{ textAlign: 'center', backgroundColor: '#4285f4' }}>
+      <iframe
+        title="Brevo Form"
+        src="https://58191143.sibforms.com/serve/MUIFAI4MgNZhTZSION7Wrpz8cuXYPZValHET18fwovE7dTM_86tDsvNR_Yt8yeFSfo-SCLGVF4ExyTEQnziFTN3EQrQ-iUQs2qkBadTxuJ6hiMoO9I8nT86stw-g69pZzGI14Z6MAxae1E6y4ElAJpMVf8obclBkD_SuRug6i0K0hoEF0NsW-oq9jgfU9nhzFwZxBw8-TiTPHcpe"
+        width="100%"
+        height="600"
+        frameBorder="0"
+        scrolling="auto"
+        style={{ display: 'block', marginLeft: 'auto', marginRight: 'auto', maxWidth: '100%' }}
+      ></iframe>
+    </div>
+  );
+};
+
+export default BrevoForm;
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -8,7 +8,48 @@ class MyDocument extends Document {
     return (
       <Html lang={AppConfig.locale}>
         <Head>
+        <style>
+            {`
+              @font-face {
+                font-display: block;
+                font-family: Roboto;
+                src: url(https://assets.brevo.com/font/Roboto/Latin/normal/normal/7529907e9eaf8ebb5220c5f9850e3811.woff2) format("woff2"), url(https://assets.brevo.com/font/Roboto/Latin/normal/normal/25c678feafdc175a70922a116c9be3e7.woff) format("woff");
+              }
+              @font-face {
+                font-display: fallback;
+                font-family: Roboto;
+                font-weight: 600;
+                src: url(https://assets.brevo.com/font/Roboto/Latin/medium/normal/6e9caeeafb1f3491be3e32744bc30440.woff2) format("woff2"), url(https://assets.brevo.com/font/Roboto/Latin/medium/normal/71501f0d8d5aa95960f6475d5487d4c2.woff) format("woff");
+              }
+              @font-face {
+                font-display: fallback;
+                font-family: Roboto;
+                font-weight: 700;
+                src: url(https://assets.brevo.com/font/Roboto/Latin/bold/normal/3ef7cf158f310cf752d5ad08cd0e7e60.woff2) format("woff2"), url(https://assets.brevo.com/font/Roboto/Latin/bold/normal/ece3a1d82f18b60bcce0211725c476aa.woff) format("woff");
+              }
+              #sib-container input:-ms-input-placeholder {
+                text-align: left;
+                font-family: Helvetica, sans-serif;
+                color: #c0ccda;
+              }
+              #sib-container input::placeholder {
+                text-align: left;
+                font-family: Helvetica, sans-serif;
+                color: #c0ccda;
+              }
+              #sib-container textarea::placeholder {
+                text-align: left;
+                font-family: Helvetica, sans-serif;
+                color: #c0ccda;
+              }
+              #sib-container a {
+                text-decoration: underline;
+                color: #2BB2FC;
+              }
+            `}
+          </style>
         <link rel="canonical" href={AppConfig.baseUrl} />
+        <link rel="stylesheet" href="https://sibforms.com/forms/end-form/build/sib-styles.css" />
         <meta name="google-site-verification" content="cUimG9-WnVYYDC8Tk1Dr6Ieh4ARJp-HzYxrbKTzYxpI" />
         <meta name="description" content="Travel like a pro with location suggestions for your maps." />
         <meta name="keywords" content="travel insurance, mobile data e-sim, mapping services, Google Maps, tourist guides, what to do" />
@@ -25,6 +66,24 @@ class MyDocument extends Document {
         <body>
           <Main />
           <NextScript />
+          <script>
+            {`
+              window.REQUIRED_CODE_ERROR_MESSAGE = 'Please choose a country code';
+              window.LOCALE = 'en';
+              window.EMAIL_INVALID_MESSAGE = window.SMS_INVALID_MESSAGE = "The information provided is invalid. Please review the field format and try again.";
+              window.REQUIRED_ERROR_MESSAGE = "This field cannot be left blank.";
+              window.GENERIC_INVALID_MESSAGE = "The information provided is invalid. Please review the field format and try again.";
+              window.translation = {
+                common: {
+                  selectedList: '{quantity} list selected',
+                  selectedLists: '{quantity} lists selected'
+                }
+              };
+              var autoHide = Boolean(1);
+            `}
+          </script>
+          <script defer src="https://sibforms.com/forms/end-form/build/main.js"></script>
+          <script src="https://www.google.com/recaptcha/api.js?hl=en"></script>
         </body>
       </Html>
     );
