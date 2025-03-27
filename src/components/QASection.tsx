47693a78669968cfc3cd43b490cc0b434678f8ff
Stavros Kalfas <kalfas@protonmail.com>
2025-03-27 13:56:11 +0200

    Refine answers in QASection

--- src/components/QASection.tsx
+++ src/components/QASection.tsx
@@ -4,11 +4,11 @@ const QASection = () => {
   const questionsAndAnswers = [
     {
       question: "What is googlementor.com?",
-      answer: "Googlementor is your trusted platform for curated lists of maps. We provide tailored advice, practical tips and fun insightful guidance to help you explore, discover and make the most out of Google Maps."
+      answer: "Googlementor is your trusted platform for curated lists of maps to discover and make the most out of Google Maps."
     },
     {
       question: "What are Googlementor lists?",
-      answer: "Googlementor lists are curated collections of saved locations organized around a specific theme. They simplify your search, making it easier to discover and explore places that match your interests. Once saved, you can find them in Google Maps under the 'You' tab -> 'Lists that you saved'."
+      answer: "Googlementor lists are curated collections of saved locations organized around a specific themes. They simplify your search, making it easier to discover and explore places that match your interests. Once saved, you can find them in Google Maps under the 'You' tab -> 'Lists that you saved'."
     },
     {
       question: "How can I use a Googlementor list for Google Maps?",
