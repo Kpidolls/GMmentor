3918e9c79b2377394bcbbe7e0b01c551b82aa163
Stavros Kalfas <kalfas@protonmail.com>
2025-06-16 21:45:25 +0300

    fix: update CNAME, homepage URL, and sitemap last modified dates for consistency

--- src/utils/AppConfig.ts
+++ src/utils/AppConfig.ts
@@ -3,5 +3,5 @@ export const AppConfig = {
   title: 'Googlementor',
   description: 'Travel like a pro with location suggestions for your maps.',
   locale: 'en',
-  baseUrl: process.env.NODE_ENV === 'production' ? 'https://kpidolls.github.io/GMmentor/' : 'http://localhost:3000',
+  baseUrl: process.env.NODE_ENV === 'production' ? 'https://googlementor.com/' : 'http://localhost:3000',
 };
