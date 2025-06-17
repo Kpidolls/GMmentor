700a0d46c052c4b2c022c721155b00db14bcb8c6
Stavros Kalfas <kalfas@protonmail.com>
2025-06-17 13:08:06 +0300

    feat(MainHero): Enhance SEO and improve UI elements
    - Added meta tags for better SEO in the MainHero component.
    - Updated the description in AppConfig for clarity.
    - Commented out the Image component for optimized background handling.
    - Improved the layout and styling of hero content, including typography adjustments.
    - Enhanced button styles for primary and secondary actions for better user experience.
    - Added a blurred background image for visual appeal.

--- src/utils/AppConfig.ts
+++ src/utils/AppConfig.ts
@@ -1,7 +1,7 @@
 export const AppConfig = {
   site_name: 'Googlementor',
   title: 'Googlementor',
-  description: 'Travel like a pro with location suggestions for your maps.',
+  description: 'Travel like a pro with Google Maps travel lists.',
   locale: 'en',
   baseUrl: process.env.NODE_ENV === 'production' ? 'https://googlementor.com/' : 'http://localhost:3000',
 };
